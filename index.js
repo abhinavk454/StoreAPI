12999185904e340afb2fda8e6ab6805d437c4ebb
Abhinav Kumar <32016468+abhinavk454@users.noreply.github.com>
2022-01-17 19:07:50 +0530

    Update Index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./db/connect");
-
 const app = express();
 
 app.use(express.json());
